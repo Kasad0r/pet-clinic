5e01b2a189adc1ba82c88def90ab1aa90fce4179
kasad0r <2431082dima>
2020-07-11 23:24:10 +0200

    Init commit

---
+++ src/test/java/kasad0r/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package kasad0r.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
