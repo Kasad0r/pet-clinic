b8e35de42b8629d6144321c988fb9bf9c37a404e
kasad0r <2431082dima>
2020-07-14 00:09:51 +0200

    refactoring to common crud service interface. Close #11

---
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/CrudService.java
@@ -0,0 +1,13 @@
+package kasad0r.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+
+    T save(T obj);
+
+    T findById(ID id);
+
+    
+}
