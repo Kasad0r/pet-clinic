7529d72fc66ffb55c83815a0c7b19d33b614c7df
kasad0r <2431082dima>
2020-07-14 14:12:37 +0200

    Create Index page and controller

---
+++ pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package kasad0r.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
