20e082b2807da0c2c1ecb9d0fbd3c95172767fb7
kasad0r <2431082dima>
2020-07-17 23:14:57 +0200

    Adding bootstrap data. Close #15

--- pet-clinic-data/src/main/java/kasad0r/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/OwnerService.java
@@ -2,8 +2,6 @@ package kasad0r.petclinic.services;
 
 import kasad0r.petclinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner,Long> {
     Owner findByLastName(String lastName);
 
--- pet-clinic-data/src/main/java/kasad0r/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,13 @@ package kasad0r.petclinic.services.map;
 
 import kasad0r.petclinic.model.Owner;
 import kasad0r.petclinic.services.CrudService;
+import kasad0r.petclinic.services.OwnerService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -32,5 +33,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     void delete(Owner obj) {
         super.delete(obj);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
