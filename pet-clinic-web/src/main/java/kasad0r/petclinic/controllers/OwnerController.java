cd557d0de37ea5409df25f3b46b8696fe0d8b0af
kasad0r <2431082dima>
2020-07-17 23:51:40 +0200

    Show owners list in index page. Close #17

--- pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package kasad0r.petclinic.controllers;
 
+import kasad0r.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
