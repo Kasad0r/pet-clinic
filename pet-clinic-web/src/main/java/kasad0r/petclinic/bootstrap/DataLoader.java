9994624ab0e505a7dfd0d62d3f224d49e88a3588
kasad0r <2431082dima>
2020-07-18 00:00:03 +0200

    Show owners list in index page. Close #16

--- pet-clinic-web/src/main/java/kasad0r/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kasad0r/petclinic/bootstrap/DataLoader.java
@@ -48,7 +48,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("Varvara");
         vet2.setLastName("Odnolubka");
 
--- pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/VetController.java
@@ -1,13 +1,21 @@
 package kasad0r.petclinic.controllers;
 
+import kasad0r.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-    private String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    private String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
