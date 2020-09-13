a35d11c529f0fe1dd8834ae9c12206077fed0cc8
= <dimon.pokemon99@gmail.com>
2020-09-13 15:54:56 +0300

    fixing broken links. close #45

--- pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/kasad0r/petclinic/controllers/IndexController.java
@@ -2,11 +2,17 @@ package kasad0r.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @GetMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
     }
 }
