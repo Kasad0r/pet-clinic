aadcd85697328c0f88559af5cee375ae3e17b51b
= <dimon.pokemon99@gmail.com>
2020-09-22 19:59:16 +0300

    implemented jpa visit service, add load date for it. close #47

---
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,52 @@
+package kasad0r.petclinic.services.springdatajpa;
+
+import kasad0r.petclinic.model.Visit;
+import kasad0r.petclinic.repositories.VisitRepository;
+import kasad0r.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author kasad0r
+ * @created 22/09/2020-19:46
+ * @project pet-clinic
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visitSet = new HashSet<>();
+        visitRepository.findAll().forEach(visitSet::add);
+        return visitSet;
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        return visitRepository.save(obj);
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
