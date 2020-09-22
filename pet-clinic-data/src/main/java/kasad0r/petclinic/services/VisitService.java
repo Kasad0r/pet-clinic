7382272a586f934f0642dce36e4a32684f5e723e
= <dimon.pokemon99@gmail.com>
2020-09-22 19:44:24 +0300

    implemented map impl for visit service. close #46

---
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package kasad0r.petclinic.services;
+
+import kasad0r.petclinic.model.Visit;
+
+/**
+ * @author kasad0r
+ * @created 22/09/2020-19:36
+ * @project pet-clinic
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/kasad0r/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package kasad0r.petclinic.services.map;
+
+import kasad0r.petclinic.model.Visit;
+import kasad0r.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author kasad0r
+ * @created 22/09/2020-19:37
+ * @project pet-clinic
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit t) {
+        super.delete(t);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        if (obj.getPet() == null || obj.getPet().getOwner() == null || obj.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(obj);
+    }
+}
